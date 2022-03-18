3e3f610eee7628639950427533b7f1300ca555cd
RamiroMagnater1 <>
2022-03-18 14:58:23 +0000

    new try

--- list.c
+++ list.c
@@ -44,11 +44,16 @@ void * firstList(List * list) {
   
   list->current = list->head;
   
-  return list->current->data;
+  return (list->current->data);
 }
 
 void * nextList(List * list) {
-    return NULL;
+
+  if(list->current->next == NULL) return NULL;
+
+  list->current->next = list->current;
+  
+  return (list->current->data);
 }
 
 void * lastList(List * list) {
