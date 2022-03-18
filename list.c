82f0cd349ce2bfe51d538728183f83ae3cf22e6a
RamiroMagnater1 <>
2022-03-18 14:36:46 +0000

    new try

--- list.c
+++ list.c
@@ -29,7 +29,13 @@ Node * createNode(void * data) {
 }
 
 List * createList() {
-     return NULL;
+
+    list * lista = (list*) malloc(sizeof(list));
+    lista->head = NULL;
+    lista->tail = NULL;
+    lista->current = NULL;
+  
+    return lista;
 }
 
 void * firstList(List * list) {
